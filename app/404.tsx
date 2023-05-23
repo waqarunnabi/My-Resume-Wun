a3d132d943e6a242c15e4fa0aa8a19de0aa54fd1
Waqar Un Nabi <syedwaqarunnabi@gmail.com>
2023-05-24 00:03:18 +0500

    .

---
+++ app/404.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const Error404 = () => {
+  return (
+    <>
+        <div id="notfound">
+            <div className="notfound">
+                <div className="notfound-404">
+                    <h1>404</h1>
+                </div>
+                <h2>We are sorry, Page not found!</h2>
+                <p>
+                    The page you are looking for might have been removed had its name
+                    changed or is temporarily unavailable.
+                </p>
+                <a href="https://waqarunnabi.vercel.app/">Back To Homepage</a>
+            </div>
+        </div>
+    
+    </>
+  )
+}
+
+
